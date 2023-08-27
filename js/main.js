ca34dc836fa97dd3c567d32edcc2ebe4a0a8e9d0
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2023-08-27 09:21:37 +0200

    JavaScript course by Dave Gray

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// strings
+
+// const myVariable = "Mathematics"
+
+const myNumber = 42;
+console.log(myNumber);
+
+
+const myFloat = 42.01;
+console.log(myFloat);
+
+// The length property
+// console.log(myVariable.length)
+
+// console.log(myVariable.charAt(5));
+
+
+// console.log(myVariable.indexOf("ema"));
+// console.log(myVariable.lastIndexOf("at"));
+// console.log(myVariable.slice(4, 6));
+// console.log(myVariable.toUpperCase());
+// console.log(myVariable.toLowerCase());
+
+
+// console.log(myVariable.includes());
+// console.log(myVariable.split());
+
